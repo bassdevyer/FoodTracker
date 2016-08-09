eb6612a0cfb8e7f6991dde5e6af6665ab8cfbf34
miguetv17 <miguetv17@gmail.com>
2016-08-09 17:03:14 -0500

    Define an Action to Perform

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    // The IBOutlet attribute tells Xcode that you can connect to the nameTextField property from Interface Builder (which is why the attribute has the IB prefix)
+    // The weak keyword means that it’s possible for that property to have no value (be nil) at some point in its life
+    @IBOutlet weak var nameTextField: UITextField!
+    // This exclamation point indicates that the type is an implicitly unwrapped optional, which is an optional type that will always have a value after the value is first set
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +27,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: Actions
+    // The sender parameter points to the object that was responsible for triggering the action
+    // The IBAction attribute indicates that the method is an action that you can connect to from your storyboard in Interface Builder
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
 
 
 }
